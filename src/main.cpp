bf3b44a5c1628e17f79e475b5cbb89fd66288c7f
jogurtas <kisielius.mykolas@gmail.com>
2019-07-21 01:23:45 +0300

    SDL_image & fixed cmake find files

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
     SDL_Event windowEvent;
 
-    imageSurface = IMG_Load("../assets/logo.png");
+    imageSurface = IMG_Load("../../assets/logo.png");
 
     if (nullptr == imageSurface) {
         std::cout << "SDL could not load image! SDL Error: " << SDL_GetError() << std::endl;
